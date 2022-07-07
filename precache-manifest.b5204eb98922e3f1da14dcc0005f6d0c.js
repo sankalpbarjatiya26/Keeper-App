58855bcde6619582ec521bb2c5c0b32402e099ba
sankalpbarjatiya26 <sankalpbarjatiya26@gmail.com>
2022-07-07 12:37:00 +0530

    Updates

---
+++ precache-manifest.b5204eb98922e3f1da14dcc0005f6d0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a86051c7a268c3f71db9fc216371589",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "d246092a4e7fc30b683d",
+    "url": "/Keeper-App/static/js/2.e447aedb.chunk.js"
+  },
+  {
+    "revision": "ad41609da9cd7f7a8536",
+    "url": "/Keeper-App/static/js/main.df848e6b.chunk.js"
+  },
+  {
+    "revision": "04feb88fc9a9fb5b1985",
+    "url": "/Keeper-App/static/js/runtime-main.3d98713c.js"
+  }
+]);
